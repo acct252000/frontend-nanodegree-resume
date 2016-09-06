b8af3685e321154c8cf46a8763bc91305f134fdf
Christine Stoner <acct252000@gmail.com>
2016-09-05 22:05:33 -0500

    Upload resume for submission

--- js/helper.js
+++ js/helper.js
@@ -1,19 +1,15 @@
 /*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
+This file contains all of the code running in the background that makes resumeBuilder.js possible. 
+Code is attributable to Cameron Pittman and Udacity course.
 */
 
 
 /*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
+These are HTML strings with the %data% or # replaced through the resumeBuilder.js file.
 */
+
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<div id="role">%data%</div><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -43,56 +39,26 @@ var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolName = '<a href="#">%data%</a>';
+var HTMLschoolDegree = ' -- %data%';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+
+var HTMLonlineClasses = '<h3 id="online-classes">Online Classes</h3>';
+var HTMLonlineTitle = '<a class="online-school" href="#">%data%';
+var HTMLonlineSchool = ' - %data%</a><br>';
+var HTMLonlineDates = '<div class="date-text online-dates">%data%</div><br>';
+
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
 
-/*
-The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
-$(document).ready(function() {
-  $('button').click(function() {
-    var $name = $('#name');
-    var iName = inName($name.text()) || function(){};
-    $name.html(iName);
-  });
-});
 
 /*
-The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
-*/
-var clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-  // your code goes here!
-});
-
-
-
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
+Here's where we generate the custom Google Map for the website.
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
@@ -102,6 +68,8 @@ var map;    // declares a global map variable
 /*
 Start here! initializeMap() is called when page is loaded.
 */
+
+
 function initializeMap() {
 
   var locations;
@@ -175,9 +143,9 @@ function initializeMap() {
       content: name
     });
 
-    // hmmmm, I wonder what this is about...
+    // Opens an infowindow with location name when user clicks on marker.
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+       infoWindow.open(map, this);
     });
 
     // this is where the pin actually gets added to the map.
@@ -234,16 +202,14 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
-
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+// and adjust map bounds for both maps
+window.addEventListener('resize', function(e) {
+  //Make sure the map bounds get updated on page resize for both maps
+  map.fitBounds(mapBounds);
+ 
+});
+
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,219 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/*General structure of file:  four objects for resume informatio are dfined bio, education, work, and projects.  Display
+function is then defined for each object.  Display functions are then called.  Lastly, a google map is adeed.*/
+
+// Define the object bio containing basic biographical information for the resume.
+var bio = {
+	"name": "Christine Stoner",
+	"role": "Independent Developer",
+	"contacts": {
+		"mobile": "(123)456 - 7890",
+		"email": "clhoward24@hotmail.com",
+		"github": "acct252000",
+		"twitter": "twitter",
+		"location": "Plano, TX"
+	},
+	"welcomeMessage": " -- Welcome to my Resume!",
+	"skills": [
+		"English Fluency","Internet Surfer"
+	],
+	"bioPic": "images/profile.svg"
+};
+
+// Define the object education containing school and online course information for the resume.
+var education = {
+	"schools": [
+			{
+				"name": "The University of Texas at Austin",
+				"location": "Austin, TX",
+				"degree": "Masters in Professional Accounting",
+				"majors": [ 
+					"Accounting", "Business Honors"
+				],
+				"dates": "1994-1998",
+				"url": "http://www.utexas.edu"
+			},
+			{
+				"name": "The Texas Academy of Math and Science",
+				"location": "Denton, TX",
+				"degree": "High School Diploma",
+				"majors": [ 
+					"Math", "Science"
+				],
+				"dates": "1992-1994",
+				"url": "https://tams.unt.edu/"
+			}
+		],
+	"onlineCourses": [
+			{
+				"title": "Front End Web Development Nanodegree",
+				"school": "Udacity",
+				"dates": "August - September 2016",
+				"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+
+			},
+			{
+				"title": "How to Use Git and GitHub",
+				"school": "Udacity",
+				"dates": "August 2016",
+				"url": "https://https://www.udacity.com/course/how-to-use-git-and-github--ud775"
+
+			},
+			{
+				"title": "Creative, Serious and Playful Science of Android Apps (no longer available)",
+				"school": "Coursera",
+				"dates": "December 2013 - February 2014",
+				"url": "https://www.coursera.org"
+
+			},
+			{
+				"title": "Learn to Program: The Fundamentals",
+				"school": "Coursera",
+				"dates": "December 2012",
+				"url": "https://www.coursera.org/learn/learn-to-program"
+			}
+		]
+	};
+
+// define the work object storing work history for the resume.
+
+var work = {
+	"jobs": [
+			{
+				"employer": "Self-Employed",
+				"title": "Independent Developer",
+				"location": "Plano, TX",
+				"dates": "2014-2016",
+				"description": "Indepedent developer of android applications"
+			},
+			{
+				"employer": "Giant Corporation",
+				"title": "Director/Controller",
+				"location": "Dallas, TX",				
+				"dates": "2009-2016",
+				"description": "Implemented significant Oracle changes include PSQL code review"
+			}
+		]
+};
+
+//Define the projects object to store historical project information for the resume.
+var projects = {
+
+	"projects": [
+			{
+				"title": "Portfolio Website",
+				"dates": "August - September, 2016",
+				"description": "Developed a portfolio website during Udacity's front-end development course.",
+				"images": [ 
+					"images/project1-1.png", "images/project1-2.png"
+				]
+			},
+			{
+				"title": "Audit Attack Lite",
+				"dates": "2014",
+				"description": "Developed Android application accounting game for ad-driven revenue.",
+				"images": [ 
+					"images/project2-1.png", "images/project2-2.png"
+				]
+			}
+		]
+	};
+
+/*This function displays bio information in the header section of the resume.  Contacts are addressed individually
+due to the individual helper varaiables in helper.js*/
+bio.display = function(){
+
+	    $("#header").prepend(HTMLheaderRole.replace("%data%",bio.role));
+	    $("#header").prepend(HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage));
+	    $("#header").prepend(HTMLheaderName.replace("%data%",bio.name));
+
+	    $("#topContacts").append(HTMLmobile.replace("%data%",bio.contacts.mobile));
+	    $("#topContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
+	    $("#topContacts").append(HTMLgithub.replace("%data%",bio.contacts.github));
+	    $("#topContacts").append(HTMLtwitter.replace("%data%",bio.contacts.twitter));
+	    $("#topContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
+	    $("#footerContacts").append(HTMLmobile.replace("%data%",bio.contacts.mobile));
+	    $("#footerContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
+	    $("#footerContacts").append(HTMLgithub.replace("%data%",bio.contacts.github));
+	    $("#footerContacts").append(HTMLtwitter.replace("%data%",bio.contacts.twitter));
+	    $("#footerContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
+	    $("#header").append(HTMLbioPic.replace("%data%",bio.bioPic));
+	    
+	    $("#header").append(HTMLskillsStart);
+	    //iterates over each skill and displays on document. Foreach not used as Array.
+	    for (var indexCount = 0; indexCount<bio.skills.length; indexCount++){
+	  		$("#skills").append(HTMLskills.replace("%data%",bio.skills[indexCount]));
+	    }
+	   
+}
+
+//This function iterates over all schools and online programs in the education object above and displays it on the resume.
+education.display = function(){
+	
+	education.schools.forEach( function(school){
+		$("#education").append(HTMLschoolStart);
+		var schoolName = HTMLschoolName.replace("%data%",school.name);
+		var schoolNameWithUrl = schoolName.replace("#",school.url);
+    	$(".education-entry:last").append(schoolNameWithUrl);
+		$(".education-entry:last").append(HTMLschoolLocation.replace("%data%",school.location));
+		$(".education-entry:last").append(HTMLschoolDates.replace("%data%",school.dates));
+		$(".education-entry:last").append("<br>");
+		$(".education-entry:last").append(HTMLschoolDegree.replace("%data%",school.degree));
+		// iterates over majors in each school.  Foreach not used as Array.
+		for (var indexCount = 0; indexCount< school.majors.length; indexCount++){
+			$(".education-entry:last").append(HTMLschoolMajor.replace("%data%",school.majors[indexCount]));
+		}
+
+	});
+	$(".education-entry:last").append(HTMLonlineClasses);
+	education.onlineCourses.forEach( function(onlineCourse){
+		var onlineTitle = HTMLonlineTitle.replace("%data%",onlineCourse.title);
+		var onlineTitleWithLink = onlineTitle.replace("#",onlineCourse.url);
+		var onlineSchool = HTMLonlineSchool.replace("%data%",onlineCourse.school);
+		var onlineSchoolLine = onlineTitleWithLink.concat(onlineSchool);
+		$(".education-entry:last").append(onlineSchoolLine);
+		$(".education-entry:last").append(HTMLonlineDates.replace("%data%",onlineCourse.dates));
+		
+	});
+	
+}
+
+
+
+//This function iterates over all jobs in the work object above and displays it on the resume.
+work.display = function(){
+	work.jobs.forEach( function(job){
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployerName = HTMLworkEmployer.replace("%data%",job.employer);
+		var formatedEmployerTitle = HTMLworkTitle.replace("%data%",job.title);
+    	var formattedEmployerNameandTitle = formattedEmployerName + formatedEmployerTitle;
+    	$(".work-entry:last").append(formattedEmployerNameandTitle);
+		$(".work-entry:last").append(HTMLworkDates.replace("%data%",job.dates));
+		$(".work-entry:last").append(HTMLworkLocation.replace("%data%",job.location));
+		$(".work-entry:last").append(HTMLworkDescription.replace("%data%",job.description));	
+	});
+}
+
+//This function iterates over all projects in the projects object above and displays it on the resume.
+projects.display = function(){
+
+	 projects.projects.forEach( function(eachProject){
+		$("#projects").append(HTMLprojectStart);
+		$(".project-entry:last").append(HTMLprojectTitle.replace("%data%",eachProject.title));
+		$(".project-entry:last").append(HTMLprojectDates.replace("%data%",eachProject.dates));
+		$(".project-entry:last").append(HTMLprojectDescription.replace("%data%",eachProject.description));
+		// iterates over images in image array and adds to display.  Foreach not used as array.
+		for(var indexCount = 0; indexCount < eachProject.images.length; indexCount++){
+			$(".project-entry:last").append(HTMLprojectImage.replace("%data%",eachProject.images[indexCount]));	
+		}
+	});
+}
+
+
+//Display all four objects using encapsulated display functions
+bio.display();
+education.display();
+work.display();
+projects.display();
+
+//Displays Google map with locations lived and worked.
+$("#mapDiv").append(googleMap);
